49aa36cabdff12c36035fe93810a7e74180e47cc
Stanley Goldman <Stanley.Goldman@gmail.com>
2018-05-30 08:24:01 -0400

    Working Demo!

--- OctoShell/Program.cs
+++ OctoShell/Program.cs
@@ -1,8 +1,7 @@
 ﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
+using System.Drawing;
+using Colorful;
+using Console = Colorful.Console;
 
 namespace OctoShell
 {
@@ -10,6 +9,51 @@ namespace OctoShell
     {
         static void Main(string[] args)
         {
+            FigletFont font = FigletFont.Load("slant.flf");
+            Figlet figlet = new Figlet(font);
+
+            Console.SetWindowSize(40, 40);
+            Console.WriteLine(figlet.ToAscii("Github"), ColorTranslator.FromHtml("#8AFFEF"));
+
+            var content = @".------------------------------------------------------------------------------.
+|                             .mmMMMMMMMMMMMMMmm.                              |
+|                         .mMMMMMMMMMMMMMMMMMMMMMMMm.                          |
+|                      .mMMMMMMMMMMMMMMMMMMMMMMMMMMMMMm.                       |
+|                    .MMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMM.                     |
+|                  .MMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMM.                   |
+|                 MMMMMMMM'  `""MMMMM""""""""""""""MMMM""""`  'MMMMMMMM                  |
+|                MMMMMMMMM                           MMMMMMMMM                 |
+|               MMMMMMMMMM:                         :MMMMMMMMMM                |
+|              .MMMMMMMMMM                           MMMMMMMMMM.               |
+|              MMMMMMMMM""                             ""MMMMMMMMM               |
+|              MMMMMMMMM                               MMMMMMMMM               |
+|              MMMMMMMMM                               MMMMMMMMM               |
+|              MMMMMMMMMM                             MMMMMMMMMM               |
+|              `MMMMMMMMMM                           MMMMMMMMMM`               |
+|               MMMMMMMMMMMM.                     .MMMMMMMMMMMM                |
+|                MMMMMMMMMMMMMMMMMM         MMMMMMMMMMMMMMMMMM                 |
+|                 MMMMMMMMMMMMMMMM           MMMMMMMMMMMMMMMM                  |
+|                  `MMMMMMMMMMMMMM           MMMMMMMMMMMMMM`                   |
+|                    `MMMMMMMMMMMM           MMMMMMMMMMMM`                     |
+|                      `""MMMMMMMMM           MMMMMMMMM""`                       |
+|                         `""MMMMMM           MMMMMM""`                          |
+|                             `""""M           M""""`                              |
+'------------------------------------------------------------------------------'";
+
+            var lines = content.Split(new [] { Environment.NewLine }, StringSplitOptions.RemoveEmptyEntries);
+
+            int r = 225;
+            int g = 255;
+            int b = 250;
+            for (int i = 0; i < lines.Length; i++)
+            {
+                Console.WriteLine(lines[i], Color.FromArgb(r, g, b));
+
+                r -= 8;
+                b -= 4;
+            }
+
+            Console.ReadLine();
         }
     }
 }
